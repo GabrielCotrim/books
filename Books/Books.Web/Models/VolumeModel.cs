a4292a9f7963b2952f7bb6693a297d0653051eab
GabrielCotrim <gabrielcotrim@outlook.com>
2020-10-30 15:32:26 -0300

    master - Adiciona favorito

--- Books/Books.Web/Models/VolumeModel.cs
+++ Books/Books.Web/Models/VolumeModel.cs
@@ -11,6 +11,7 @@ namespace Books.Web.Models
         public string Titulo { get; set; }
         public string Resumo { get; set; }
         public string ImageLink { get; set; }
+        public bool EhFavorito { get; set; }
         public string ResumoAbreviado => Resumo.Length > 300
                                          ? Resumo.Substring(0, 297)+"..."
                                          : Resumo;
